a003198f6d5b6d945166fb73fb5a65fa75e6bfcd
Ghosterers <algorithmallies.dis.ghost@gmail.com>
2025-08-30 12:34:16 +0200

    fix guild commands

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,6 +1,17 @@
 import "dotenv/config";
 import { REST, Routes } from "discord.js";
 import social from "./commands/social.js";
+
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
-await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID!), { body: [social.data.toJSON()] });
-console.log("Slash commands registrati");
+const appId = process.env.DISCORD_CLIENT_ID!;
+const guildId = process.env.GUILD_ID;
+
+const body = [social.data.toJSON()];
+
+if (guildId) {
+  await rest.put(Routes.applicationGuildCommands(appId, guildId), { body });
+  console.log(`Registered ${body.length} guild command(s) to ${guildId}`);
+} else {
+  await rest.put(Routes.applicationCommands(appId), { body });
+  console.log(`Registered ${body.length} global command(s)`);
+}
